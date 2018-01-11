cc35f748642e131f111947485dcce36e83ccd14c
eiki-tabayashi <eiki.tabayashi@gmail.com>
2018-01-11 17:35:46 +0900

    today

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
 'use strict';
 var gulp = require('gulp');
-gulp.task('default', function(done) {
+var util = require('gulp-util');
+
+function first(done) {
+  util.log('First task done !');
   done();
-});
+}
+
+function second(done) {
+  util.log('Second task done !');
+  done();
+}
+
+function third(done) {
+  util.log('Third task done !');
+  done();
+}
+
+function parallel(done) {
+  util.log('Parallel task done !');
+  done();
+}
+
+function series(done) {
+  util.log('Series task done !');
+  done();
+}
+
+gulp.task('first', first);
+gulp.task('second', second);
+gulp.task('third', third);
+gulp.task('default', gulp.series(
+  'third',
+  gulp.parallel('first',);
+  
+  'second',
+  'parallel'
+));
